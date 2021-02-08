9d455534ca9796844f12b6841082ed23d9b7d689
rustam023 <r.91@mail.ru>
2021-02-09 00:41:40 +0300

    add q uniqueness validation test in model

--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -4,6 +4,7 @@ RSpec.describe Question, type: :model do
   context 'validations check' do
     it { should validate_presence_of :level }
     it { should validate_presence_of :text }
+    it { should validate_uniqueness_of :text}
     it { should validate_inclusion_of(:level).in_range(0..14) }
 
     it { should_not allow_value(500).for(:level) }
