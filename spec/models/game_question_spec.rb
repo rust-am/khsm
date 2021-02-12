b1d56502e97cbcb04dec6709fde876fed05d9787
rustam023 <r.91@mail.ru>
2021-02-12 20:51:38 +0300

    add .help_hash test

--- spec/models/game_question_spec.rb
+++ spec/models/game_question_spec.rb
@@ -29,6 +29,24 @@ RSpec.describe GameQuestion, type: :model do
     end
   end
 
+  it 'correct .help_hash' do
+    # на фабрике у нас изначально хэш пустой
+    expect(game_question.help_hash).to eq({})
+
+    # добавляем пару ключей
+    game_question.help_hash[:some_key1] = 'blabla1'
+    game_question.help_hash['some_key2'] = 'blabla2'
+
+    # сохраняем модель и ожидаем сохранения хорошего
+    expect(game_question.save).to be_truthy
+
+    # загрузим этот же вопрос из базы для чистоты эксперимента
+    gq = GameQuestion.find(game_question.id)
+
+    # проверяем новые значение хэша
+    expect(gq.help_hash).to eq({some_key1: 'blabla1', 'some_key2' => 'blabla2'})
+  end
+
   # help_hash у нас имеет такой формат:
   # {
   #   fifty_fifty: ['a', 'b'], # При использовании подсказски остались варианты a и b
