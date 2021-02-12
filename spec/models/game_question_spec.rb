47da72328c9cc0ceb0869bae4739bc0939aa57d3
rustam023 <r.91@mail.ru>
2021-02-12 20:58:15 +0300

    add fifty_fifty test

--- spec/models/game_question_spec.rb
+++ spec/models/game_question_spec.rb
@@ -47,6 +47,21 @@ RSpec.describe GameQuestion, type: :model do
     expect(gq.help_hash).to eq({some_key1: 'blabla1', 'some_key2' => 'blabla2'})
   end
 
+  # проверяем работу 50/50
+  it 'correct fifty_fifty' do
+    # сначала убедимся, в подсказках пока нет нужного ключа
+    expect(game_question.help_hash).not_to include(:fifty_fifty)
+    # вызовем подсказку
+    game_question.add_fifty_fifty
+
+    # проверим создание подсказки
+    expect(game_question.help_hash).to include(:fifty_fifty)
+    ff = game_question.help_hash[:fifty_fifty]
+
+    expect(ff).to include('b') # должен остаться правильный вариант
+    expect(ff.size).to eq 2 # всего должно остаться 2 варианта
+  end
+
   # help_hash у нас имеет такой формат:
   # {
   #   fifty_fifty: ['a', 'b'], # При использовании подсказски остались варианты a и b
