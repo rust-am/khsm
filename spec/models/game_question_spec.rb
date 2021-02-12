706d39e4cf302b86b1c84d77312d8758130a59cc
rustam023 <r.91@mail.ru>
2021-02-12 21:22:10 +0300

    add friend_call test

--- spec/models/game_question_spec.rb
+++ spec/models/game_question_spec.rb
@@ -24,42 +24,9 @@ RSpec.describe GameQuestion, type: :model do
   end
 
   describe '#correct_answer_key' do
-    it 'should return correct answer what is equal to b variant'do
+    it 'should return correct answer what is equal to b variant' do
       expect(game_question.correct_answer_key).to eq("b")
     end
-  end
-
-  it 'correct .help_hash' do
-    # на фабрике у нас изначально хэш пустой
-    expect(game_question.help_hash).to eq({})
-
-    # добавляем пару ключей
-    game_question.help_hash[:some_key1] = 'blabla1'
-    game_question.help_hash['some_key2'] = 'blabla2'
-
-    # сохраняем модель и ожидаем сохранения хорошего
-    expect(game_question.save).to be_truthy
-
-    # загрузим этот же вопрос из базы для чистоты эксперимента
-    gq = GameQuestion.find(game_question.id)
-
-    # проверяем новые значение хэша
-    expect(gq.help_hash).to eq({some_key1: 'blabla1', 'some_key2' => 'blabla2'})
-  end
-
-  # проверяем работу 50/50
-  it 'correct fifty_fifty' do
-    # сначала убедимся, в подсказках пока нет нужного ключа
-    expect(game_question.help_hash).not_to include(:fifty_fifty)
-    # вызовем подсказку
-    game_question.add_fifty_fifty
-
-    # проверим создание подсказки
-    expect(game_question.help_hash).to include(:fifty_fifty)
-    ff = game_question.help_hash[:fifty_fifty]
-
-    expect(ff).to include('b') # должен остаться правильный вариант
-    expect(ff.size).to eq 2 # всего должно остаться 2 варианта
   end
 
   # help_hash у нас имеет такой формат:
@@ -79,6 +46,49 @@ RSpec.describe GameQuestion, type: :model do
 
       ah = game_question.help_hash[:audience_help]
       expect(ah.keys).to contain_exactly('a', 'b', 'c', 'd')
+    end
+
+    it 'correct .help_hash' do
+      # на фабрике у нас изначально хэш пустой
+      expect(game_question.help_hash).to eq({})
+
+      # добавляем пару ключей
+      game_question.help_hash[:some_key1] = 'blabla1'
+      game_question.help_hash['some_key2'] = 'blabla2'
+
+      # сохраняем модель и ожидаем сохранения хорошего
+      expect(game_question.save).to be_truthy
+
+      # загрузим этот же вопрос из базы для чистоты эксперимента
+      gq = GameQuestion.find(game_question.id)
+
+      # проверяем новые значение хэша
+      expect(gq.help_hash).to eq({some_key1: 'blabla1', 'some_key2' => 'blabla2'})
+    end
+
+    # проверяем работу 50/50
+    it 'correct fifty_fifty' do
+      # сначала убедимся, в подсказках пока нет нужного ключа
+      expect(game_question.help_hash).not_to include(:fifty_fifty)
+      # вызовем подсказку
+      game_question.add_fifty_fifty
+
+      # проверим создание подсказки
+      expect(game_question.help_hash).to include(:fifty_fifty)
+      ff = game_question.help_hash[:fifty_fifty]
+
+      expect(ff).to include('b') # должен остаться правильный вариант
+      expect(ff.size).to eq 2 # всего должно остаться 2 варианта
+    end
+
+    it 'correct friend_call' do
+      expect(game_question.help_hash).not_to include(:friend_call)
+      game_question.add_friend_call
+
+      expect(game_question.help_hash).to include(:friend_call)
+      fc = game_question.help_hash[:friend_call]
+
+      expect(fc).to include(' считает, что это вариант ')
     end
   end
 end
