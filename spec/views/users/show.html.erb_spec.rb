11cec059be4ef69837e9fa1448b4fc28e7b2e88c
rustam023 <r.91@mail.ru>
2021-02-13 17:53:16 +0300

    add tests for users/show view

---
+++ spec/views/users/show.html.erb_spec.rb
@@ -0,0 +1,55 @@
+require 'rails_helper'
+
+RSpec.describe 'users/show', type: :view do
+  let(:game) { FactoryGirl.build_stubbed(:game, id: 10, created_at: Time.parse('2019.10.09, 13:00'), current_level: 5, prize: 1000) }
+  let(:user1) { FactoryGirl.create(:user, name: 'user1', balance: 32_000) }
+  let(:user2) { FactoryGirl.create(:user, name: 'user2', balance: 2_000) }
+
+  context 'when user is Anon' do
+    before(:each) do
+      assign(:user, user1)
+      assign(:game, game)
+
+      render
+    end
+
+    it 'show user name' do
+      expect(rendered).to match 'user1'
+    end
+
+    it 'show profile editing button' do
+      expect(rendered).not_to match 'Сменить имя и пароль'
+    end
+
+    it 'show game elements' do
+      render partial: 'users/game', object: game
+
+      expect(rendered).to match '10'
+      expect(rendered).to match '09 окт., 13:00'
+    end
+  end
+
+  context 'when user in logged in' do
+    before(:each) do
+      sign_in user1
+      assign(:user, user1)
+      assign(:game, game)
+
+      render
+    end
+
+    it 'show user name' do
+      expect(rendered).to match('user1')
+    end
+
+    it 'profile editing button is not shown' do
+      expect(rendered).to match('Сменить имя и пароль')
+    end
+
+    it 'show game elements' do
+      render partial: 'users/game', object: game
+      expect(rendered).to match '10'
+      expect(rendered).to match '09 окт., 13:00'
+    end
+  end
+end
