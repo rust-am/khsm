af14cc1445129d1dd76dd0ed0cf43761434eda8c
rustam023 <r.91@mail.ru>
2021-02-14 13:37:33 +0300

    modified 'user visits other user' feature tests

--- spec/features/user_visits_other_user_profile_spec.rb
+++ spec/features/user_visits_other_user_profile_spec.rb
@@ -7,8 +7,8 @@ RSpec.feature 'USER visits other user profile', type: :feature do
 
   let!(:games) do
     [
-      FactoryGirl.create(:game, id: 23, user: user2, created_at: Time.parse('2021.01.05, 13:00'), finished_at: Time.parse('2021.01.05, 13:30'),  current_level: 7, prize: 4_000),
-      FactoryGirl.create(:game, id: 32, user: user2, created_at: Time.parse('2021.02.11, 14:00'), current_level: 11)
+      FactoryGirl.create(:game, user: user2, created_at: Time.parse('2021.01.05, 13:00'), finished_at: Time.parse('2021.01.05, 13:30'),  current_level: 7, prize: 4_000),
+      FactoryGirl.create(:game, user: user2, created_at: Time.parse('2021.02.11, 14:00'), current_level: 14)
     ]
   end
 
@@ -27,17 +27,16 @@ RSpec.feature 'USER visits other user profile', type: :feature do
     expect(page).to have_content 'второй'
     expect(page).not_to have_content 'Сменить имя и пароль'
 
-    expect(page).to have_content '23'
+    expect(page).to have_content "#{games[0].id}"
     expect(page).to have_content '05 янв., 13:00'
     expect(page).to have_content 'деньги'
-    expect(page).to have_content '5'
+    expect(page).to have_content '7'
     expect(page).to have_content '4 000 ₽'
 
-    expect(page).to have_content '32'
+    expect(page).to have_content "#{games[1].id}"
     expect(page).to have_content '11 февр., 14:00'
     expect(page).to have_content 'в процессе'
-    expect(page).to have_content '6'
+    expect(page).to have_content '14'
     expect(page).to have_content '0 ₽'
   end
 end
-
